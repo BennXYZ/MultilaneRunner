270a60f3d359271f6023e020407654efd177aa71
Benedikt Zoeller <sephirot5@web.de>
2017-03-27 16:14:28 +0200

    Working on Hazards & Rotation

---
+++ Endless Runner/Assets/Scripts/GroundMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(-6 * Time.deltaTime, 0, 0));
+    }
+}
