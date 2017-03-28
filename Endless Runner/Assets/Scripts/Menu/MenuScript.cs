2cb9f59e4d247e9c64509757615f568afe417f85
icsti1379 <it.icsti@googlemail.com>
2017-03-28 20:56:22 +0200

    Added MenuScript

---
+++ Endless Runner/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Time.timeScale = 1;	
+	}
+	
+    public void Reload()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    	
+	}
+}
