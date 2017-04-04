b9965d6f749cd22d142ccff580a50187dac55b0c
Benedikt Zoeller <sephirot5@web.de>
2017-04-04 17:56:59 +0200

    Player Dash

--- Game/Endless Runner App/Assets/Scripts/Player/Player.cs
+++ Game/Endless Runner App/Assets/Scripts/Player/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour
     [SerializeField]
     float moveSpeed;
 
+    [Range(0,10)]
+    [SerializeField]
+    float dashForce;
+
     Vector2 velocity;
 
     GameObject playerPositioner;
@@ -40,9 +44,13 @@ public class Player : MonoBehaviour
     {
         if (playerPositioner != null)
         {
-            if (transform.position.x >= playerPositioner.transform.position.x + 0.05f || transform.position.x <= playerPositioner.transform.position.x - 0.05f)
+            if (transform.position.x >= playerPositioner.transform.position.x + 0.05f)
             {
-                velocity += new Vector2((playerPositioner.transform.position.x - transform.position.x) / 20, 0);
+                transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+            }
+            else if(transform.position.x <= playerPositioner.transform.position.x - 0.05f)
+            {
+                transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
             }
             else if (transform.position.x != playerPositioner.transform.position.x && playerPositioner != null)
             {
@@ -50,7 +58,7 @@ public class Player : MonoBehaviour
                 velocity = Vector2.zero;
             }
             transform.Translate(velocity.x, velocity.y, 0);
-            velocity = velocity * 0.5f;
+            velocity = velocity * (1 - dashForce * 0.1f);
         }
     }
 
@@ -72,6 +80,6 @@ public class Player : MonoBehaviour
 
     public void DashRight()
     {
-        velocity += new Vector2(2, 0);
+        velocity += new Vector2(dashForce, 0);
     }
 }
