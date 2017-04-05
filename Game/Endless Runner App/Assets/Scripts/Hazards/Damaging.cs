b7d25fb1b264671d1a31b374aafee667eeb86829
Benedikt Zoeller <sephirot5@web.de>
2017-04-05 10:24:15 +0200

    Health-System

---
+++ Game/Endless Runner App/Assets/Scripts/Hazards/Damaging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damaging : MonoBehaviour {
+
+    [SerializeField]
+    int damage;
+
+    UnityEvent hit;
+
+	// Use this for initialization
+	void Start () {
+        hit.AddListener(delegate { GameObject.FindGameObjectWithTag("Player").GetComponent<HealthManager>().Damage(1); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            hit.Invoke();
+        }
+    }
+}
--- Game/Endless Runner App/Assets/Scripts/Player/HealthManager.cs
+++ Game/Endless Runner App/Assets/Scripts/Player/HealthManager.cs
@@ -1,16 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HealthManager : MonoBehaviour {
 
+    [SerializeField]
+    int startHealth;
+
+    [SerializeField]
+    int maxHealth;
+
+    [SerializeField]
+    UnityEvent Death;
+
+    [SerializeField]
+    UnityEvent Hurt;
+
+    [SerializeField]
+    UnityEvent Healed;
+
+    int health;
+
 	// Use this for initialization
 	void Start () {
-		
+        health = startHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void Heal(int healing)
+    {
+        if (health + healing >= maxHealth)
+            health = maxHealth;
+        else
+            health += healing;
+    }
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Death.Invoke();
+        }
+    }
 }
