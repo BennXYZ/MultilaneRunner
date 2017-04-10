da4b323e36e73c0546477f771afc2485afa4e1dc
Benedikt Zoeller <sephirot5@web.de>
2017-04-10 15:20:38 +0200

    HealthDisplay

---
+++ Game/Endless Runner App/Assets/Scripts/Player/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    [SerializeField]
+    Sprite[] HealthSprites;
+
+    [SerializeField]
+    SpriteRenderer renderer;
+
+    [SerializeField]
+    HealthManager manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateHealth()
+    {
+        if (manager.Health() >= 0)
+        renderer.sprite = HealthSprites[manager.Health()];
+    }
+}
