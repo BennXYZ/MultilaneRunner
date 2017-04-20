5c0f97fcf2fb28920513493f47a9e24d7e5eb257
Benedikt Zoeller <sephirot5@web.de>
2017-04-20 10:14:26 +0200

    Upgrad Menues

---
+++ Game/Endless Runner App/Assets/Scripts/UI/UpgradeButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UpgradeButton: MonoBehaviour {
+
+    UnityEvent buttonPressed;
+
+    enum Status { Health, Strength}
+
+    [SerializeField]
+    Status currentStatus;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TryUpgrade()
+    {
+        switch (currentStatus)
+        {
+            case Status.Health:
+                GameObject.FindGameObjectWithTag("UpgradeManager").GetComponent<UpgradeScript>().TryUpgradeHealth();
+                break;
+            case Status.Strength:
+                GameObject.FindGameObjectWithTag("UpgradeManager").GetComponent<UpgradeScript>().TryUpgradeShot();
+                break;
+        }
+
+    }
+}
