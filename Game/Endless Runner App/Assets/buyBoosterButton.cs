7b8e274ff51a5299d2b3f2ac15241e86f4162a0b
Benedikt Zoeller <sephirot5@web.de>
2017-05-04 18:18:52 +0200

    working on Boosters

---
+++ Game/Endless Runner App/Assets/buyBoosterButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buyBoosterButton : MonoBehaviour {
+
+    [SerializeField]
+    int prize;
+
+    [SerializeField]
+    int maxAmount;
+
+    enum States { CoinBoost, SpeedBoost}
+
+    [SerializeField]
+    States boostState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TryBuyBooster()
+    {
+        switch(boostState)
+        {
+            case States.CoinBoost:
+                if(PlayerPrefs.GetInt("CoinBoosts",0) < maxAmount && PlayerPrefs.GetInt("Score",0) >= prize)
+                {
+                    PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score", 0) - prize);
+                    PlayerPrefs.SetInt("CoinBoosts", PlayerPrefs.GetInt("CoinBoosts", 0) + 1);
+                    Debug.Log("Bought");
+                }
+                break;
+            case States.SpeedBoost:
+                if (PlayerPrefs.GetInt("SpeedBoosts", 0) < maxAmount && PlayerPrefs.GetInt("Score", 0) >= prize)
+                {
+                    PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score", 0) - prize);
+                    PlayerPrefs.SetInt("SpeedBoosts", PlayerPrefs.GetInt("SpeedBoosts", 0) + 1);
+                    Debug.Log("Bought");
+                }
+                break;
+        }
+    }
+}
---
+++ Game/Endless Runner App/Assets/useBoosterButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class useBoosterButton : MonoBehaviour {
+
+    [SerializeField]
+    UnityEvent used;
+
+    [SerializeField]
+    string PrefsName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TryUsingBoost()
+    {
+        if(PlayerPrefs.GetInt(PrefsName,0) > 0)
+        {
+            PlayerPrefs.SetInt(PrefsName, PlayerPrefs.GetInt(PrefsName, 0) - 1);
+            used.Invoke();
+        }
+    }
+}
