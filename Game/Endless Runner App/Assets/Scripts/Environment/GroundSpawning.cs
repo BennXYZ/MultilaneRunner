c05f07d0005c798a1b439807c5bf6a4332e28777
Sebastian <sebastian@michael-bos.de>
2017-05-11 16:44:11 +0200

    Fixed GroundSpawning

--- Game/Endless Runner App/Assets/Scripts/Environment/GroundSpawning.cs
+++ Game/Endless Runner App/Assets/Scripts/Environment/GroundSpawning.cs
@@ -26,6 +26,9 @@ public class GroundSpawning : MonoBehaviour
 
     MovementManager manager;
 
+    [SerializeField]
+    bool isBackgroundSpawner;
+
     // Use this for initialization
     void Start()
     {
@@ -50,83 +53,94 @@ public class GroundSpawning : MonoBehaviour
 
     private bool SpawnNextGround()
     {
-        if(!Boss)
+        if(isBackgroundSpawner)
         {
-            //    int fullChance = 0;
-
-            //    for (int i = 0; i < blockChance.Length; i++)
-            //        fullChance += blockChance[i];
-
-            //    float Chance = Random.value * fullChance;
-            //    float currentChance = 0;
-
-            //    for (int i = 0; i < groundTiles.Length; i++)
-            //    {
-            //        currentChance += blockChance[i];
-            //        if (currentChance >= Chance)
-            //        {
-            //            GameObject nextBlock = GameObject.Instantiate(groundTiles[i],
-            //                new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
-            //                transform.rotation, transform.parent);
-            //            //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
-            //            nextBlock.name = groundTiles[i].name;
-            //            return true;
-            //        }
-            //    }
-
-            //    GameObject Catch = GameObject.Instantiate(groundTiles[groundTiles.Length - 1],
-            //new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
-            //transform.rotation, transform.parent);
-            //    Catch.name = groundTiles[groundTiles.Length - 1].name;
-            //    //Catch.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
-
-            int bosses = GameObject.Find("ScoreManager").GetComponent<acutalScoreScript>().GetBosses();
-            int min = 0;
-            int max;
-
-            switch(bosses)
-            {
-                case 0:
-                    max = 4;
-                    break;
-                case 1:
-                    max = 6;
-                    break;
-                case 2:
-                    max = 9;
-                    break;
-                case 3:
-                    max = 13;
-                    break;
-                default:
-                    max = 19;
-                    break;
-            }
-
-            int toSpawnIndex = Random.Range(min, max);
-
-            Debug.Log(min);
-            Debug.Log(toSpawnIndex);
-
-            Debug.Log(groundTiles[0]);
-            GameObject nextBlock = GameObject.Instantiate(groundTiles[toSpawnIndex],
-            new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z), transform.rotation, transform.parent);
-            Debug.Log(nextBlock);
+            GameObject nextBlock = GameObject.Instantiate(groundTiles[0], new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z), transform.rotation, transform.parent);
             //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
             //nextBlock.name = groundTiles[toSpawnIndex].name;
             return true;
         }
         else
         {
-            int toSpawnIndex = Random.Range(19, 23);
+            if (!Boss)
+            {
+                //    int fullChance = 0;
 
-            GameObject nextBlock = GameObject.Instantiate(groundTiles[toSpawnIndex],
-                        new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
-                        transform.rotation, transform.parent);
-            //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
-            nextBlock.name = groundTiles[toSpawnIndex].name;
-            nextBlock.GetComponent<GroundSpawning>().SetBoss(true);
-            return true;
+                //    for (int i = 0; i < blockChance.Length; i++)
+                //        fullChance += blockChance[i];
+
+                //    float Chance = Random.value * fullChance;
+                //    float currentChance = 0;
+
+                //    for (int i = 0; i < groundTiles.Length; i++)
+                //    {
+                //        currentChance += blockChance[i];
+                //        if (currentChance >= Chance)
+                //        {
+                //            GameObject nextBlock = GameObject.Instantiate(groundTiles[i],
+                //                new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
+                //                transform.rotation, transform.parent);
+                //            //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
+                //            nextBlock.name = groundTiles[i].name;
+                //            return true;
+                //        }
+                //    }
+
+                //    GameObject Catch = GameObject.Instantiate(groundTiles[groundTiles.Length - 1],
+                //new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
+                //transform.rotation, transform.parent);
+                //    Catch.name = groundTiles[groundTiles.Length - 1].name;
+                //    //Catch.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
+
+                int bosses = GameObject.Find("ScoreManager").GetComponent<acutalScoreScript>().GetBosses();
+                int min = 0;
+                int max;
+
+                switch (bosses)
+                {
+                    case 0:
+                        max = 4;
+                        break;
+                    case 1:
+                        max = 6;
+                        break;
+                    case 2:
+                        max = 9;
+                        break;
+                    case 3:
+                        max = 13;
+                        break;
+                    default:
+                        max = 19;
+                        break;
+                }
+
+                int toSpawnIndex = Random.Range(min, max);
+
+                Debug.Log(min);
+                Debug.Log(toSpawnIndex);
+
+                Debug.Log(groundTiles[0]);
+                GameObject nextBlock = GameObject.Instantiate(groundTiles[toSpawnIndex],
+                new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z), transform.rotation, transform.parent);
+                Debug.Log(nextBlock);
+                //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
+                //nextBlock.name = groundTiles[toSpawnIndex].name;
+                return true;
+            }
+            else
+            {
+                int toSpawnIndex = Random.Range(19, 23);
+
+                GameObject nextBlock = GameObject.Instantiate(groundTiles[toSpawnIndex],
+                            new Vector3(transform.localPosition.x + OffSet.x, transform.localPosition.y + OffSet.y, transform.localPosition.z),
+                            transform.rotation, transform.parent);
+                //nextBlock.transform.Translate(manager.Direction * manager.Speed * Time.deltaTime);
+                nextBlock.name = groundTiles[toSpawnIndex].name;
+                nextBlock.GetComponent<GroundSpawning>().SetBoss(true);
+                return true;
+            }
         }
+        
     }
 }
